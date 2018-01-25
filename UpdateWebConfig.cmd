4193fe79f5b2da65d69545a65b6168082f27eb30
Demian Lindner <dlindner@elogicgroup.com>
2018-01-25 16:56:46 -0500

    y

--- UpdateWebConfig.cmd
+++ UpdateWebConfig.cmd
@@ -1,3 +1,3 @@
 echo "Updating the web.config file"
-sed -i.bak 's/\<\/system.webServer\> /\<staticContent\>\<mimeMap fileExtension=".json" mimeType="application\/json" \/\>\<\/staticContent\>\<\/system.webServer\>' ./../wwwroot/web.config
+sed -i.bak 's/<\/system.webServer> /<staticContent><mimeMap fileExtension=".json" mimeType="application\/json" \/><\/staticContent><\/system.webServer>' ./../wwwroot/web.config
 ::rm -f ./../wwwroot/web.config.bak
